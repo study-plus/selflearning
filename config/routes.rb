dd91064dab9d60c75a152401db38f07c2a5f770d
hiroaka1250 <u957219i@ecs.osaka-u.ac.jp>
2021-06-15 23:39:56 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/edit'
+  get 'comments/update'
+  get 'likes/create'
+  get 'likes/destroy'
   devise_for :users
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
