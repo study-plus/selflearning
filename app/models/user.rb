4cfecd41a592ea84a189e30c95dd52eb7bd67fc6
hiroaka0208 <u957219i@fbs.osaka-u.ac.jp>
2021-06-19 18:32:29 +0900

    chance rails version 2.7.2 to 2.7.3in Gemfile. make relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :comments, dependent: :destroy
+
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
 end
