74a16031d0190c638795517d9de454cfdfb105ee
hiroaka1250 <u957219i@ecs.osaka-u.ac.jp>
2021-06-26 23:29:31 +0900

    ログイン機能のデバック

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_contoller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
     
     # 新規登録後のリダイレクト先をマイページへ
     def after_sign_in_path_for(resource)
         if current_user
             flash[:notice] = "ログインに成功しました" 
-            root_path  #　指定したいパスに変更
+            root_path  #指定したいパスに変更
         else
             flash[:notice] = "新規登録完了しました。次に名前を入力してください" 
-            user_path  #　指定したいパスに変更
+            user_path  #指定したいパスに変更
         end
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :trackable
 
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
