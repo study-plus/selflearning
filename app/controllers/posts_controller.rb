b8ee52e55a6f182c5763dc177fe2bbe6cbfa999f
hiroaka1250 <u957219i@ecs.osaka-u.ac.jp>
2021-07-23 01:27:47 +0900

    レイアウトの変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,15 @@ class PostsController < ApplicationController
   def index
     if params[:search] == nil
       @posts = Post.all
+      @myposts = Post.where(user_id: current_user.id)
     elsif params[:search] == ''
       @posts = Post.all
+      @myposts = Post.where(id: current_user.id)
     else
       @posts =   Post.where("subject_name LIKE ? OR content LIKE ? ",'%'+params[:search]+'%','%'+params[:search]+'%')
+      @myposts = Post.where(id: current_user.id).where("subject_name LIKE ? OR content LIKE ? ",'%'+params[:search]+'%','%'+params[:search]+'%')
     end
-    
+    @users = User.all
   end
 
   def new
@@ -18,7 +21,7 @@ class PostsController < ApplicationController
   def create
     post = Post.new(post_params)
     post.user_id = current_user.id
-    post.toal = post.hour + (post.minutes/60)
+    post.total = post.hour + (post.minutes/60)
     if post.save
         redirect_to posts_path
     else
