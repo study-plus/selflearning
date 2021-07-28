5ab216bf1a62b06847b94f32c5b5535c2b970ea5
hiroaka1250 <u957219i@ecs.osaka-u.ac.jp>
2021-07-28 21:49:33 +0900

    プロフィール編集画面の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,4 +28,22 @@ class UsersController < ApplicationController
         @events = Event.where(user_id: @user.id)
         @event = Event.new
     end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        user = User.find(params[:id])
+        if user.update(user_params)
+            redirect_to user_path(user.id)
+        else
+            redirect_to edit_user_resistration
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name,:profile)
+    end
 end
